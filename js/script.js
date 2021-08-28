c44613ff1a651b79c2f838dc6ff700e708a61e40
Wagner Trezub <trezub.w@gmail.com>
2021-08-28 18:41:44 +0200

    alteração no script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function(){
   $("nav").on("click", function(){
     $("nav ul, .menu-hamburger span:nth-child(1), .menu-hamburger span:nth-child(2), .menu-hamburger span:nth-child(3)").toggleClass("active");
-    })
+    });
   });
