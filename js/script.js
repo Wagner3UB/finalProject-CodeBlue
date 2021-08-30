2f2797f4498eb05b1bcb612fff59a4a33385925e
Wagner Trezub <trezub.w@gmail.com>
2021-08-30 16:45:15 +0200

    section learn more acrescentada e criada para desk

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ $(function(){
   $("nav").on("click", function(){
     $("nav ul, .menu-hamburger span:nth-child(1), .menu-hamburger span:nth-child(2), .menu-hamburger span:nth-child(3)").toggleClass("active");
     });
-  });
+    $(".see_more").on("click", function(){
+      $(".m-hide").removeClass("m-hide");
+      $(".see_more").addClass("active");
+    });
+});
